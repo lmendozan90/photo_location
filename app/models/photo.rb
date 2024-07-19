3dba236b59898e553cdceb27af3203eefe40f2a5
lmendozan90 <lmendozan90@gmail.com>
2024-07-19 11:32:26 -0500

    Extract metadata after saving the photo

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,3 +1,20 @@
+require "exifr/jpeg"
+
 class Photo < ApplicationRecord
-  has_one_attached :photo
+  has_one_attached :image
+
+  after_save :extract_metadata
+
+  private
+  def extract_metadata
+    return unless image.attached?
+
+    image_path = ActiveStorage::Blob.service.send(:path_for, image.key)
+    exif = EXIFR::JPEG.new(image_path)
+
+    if exif.gps
+      self.latitude = exif.gps.latitude
+      self.longitude = exif.gps.longitude
+    end
+  end
 end
